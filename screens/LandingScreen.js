545aed5ded1a4d5447b6dd2e3759882759b0046a
Drailan John Terrible <drailanjohn.gss@gmail.com>
2018-04-25 21:45:13 +0800

    update for login component

--- screens/LandingScreen.js
+++ screens/LandingScreen.js
@@ -1,22 +1,58 @@
 import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
-import { Container, Text, Content } from 'native-base';
+import {
+	Container,
+	Text,
+	Content,
+	Card,
+	CardItem,
+	Input,
+	Button
+} from 'native-base';
 
 class LandingScreen extends Component {
 	render() {
 		return (
 			<Container style={styles.container}>
-				<Text>EMMUNIZE</Text>
+				<Container>
+					<Text style={styles.textStyle}>EMMUNIZE</Text>
+				</Container>
+				<Container style={styles.container}>
+					<Card style={{ width: '100%' }}>
+						<CardItem>
+							<Text> Username: </Text>
+							<Input placeholder="input username" />
+						</CardItem>
+						<CardItem>
+							<Text> Password: </Text>
+							<Input placeholder="********" />
+						</CardItem>
+						<CardItem>
+							<Button block full primary style={{ width: '100%' }}>
+								<Text>Sign In </Text>{' '}
+							</Button>
+						</CardItem>
+						<CardItem>
+							<Button transparent block full primary style={{ width: '100%' }}>
+								<Text>Register </Text>{' '}
+							</Button>
+						</CardItem>
+					</Card>
+				</Container>
 			</Container>
 		);
 	}
 }
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
 		backgroundColor: '#fff',
 		alignItems: 'center',
-		justifyContent: 'center'
+		justifyContent: 'center',
+		width: '100%'
+	},
+	textStyle: {
+		fontSize: 50,
+		paddingTop: 30
 	}
 });
 
