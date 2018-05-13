cec9bbf5f23cee8b747f5d41ce3aa3be2ba10281
Drailan John Terrible <drailanjohn.gss@gmail.com>
2018-05-13 22:46:37 +0800

    initialized firestore

---
+++ Config/firebase.js
@@ -0,0 +1,10 @@
+export default {
+	Firebase: {
+		apiKey: 'AIzaSyDHPpm1GgJsfmt8L4Klw31FhlA0bEJZvCQ',
+		authDomain: 'emmunize-51ecf.firebaseapp.com',
+		databaseURL: 'https://emmunize-51ecf.firebaseio.com',
+		projectId: 'emmunize-51ecf',
+		storageBucket: 'emmunize-51ecf.appspot.com',
+		messagingSenderId: '646372685434'
+	}
+};
