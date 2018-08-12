cffaf910d1745ad4823029fa3d5ef17da5c0dd4a
Drailan John Terrible <drailanjohn.gss@gmail.com>
2018-08-12 15:41:10 +0800

    save scheduling

--- components/scheduling.js
+++ components/scheduling.js
@@ -4,30 +4,45 @@ import Moment from 'moment';
 
 export default class Scheduling {
     static createSchedule(birthday) {
+        // get login user credentials
         let user = firebase.auth().currentUser;
         let uuid = user.uid;
-        console.log(uuid);
+
         const db = firebase.firestore();
         // get vaccination list
-
+        let newSchedArr = [];
         db
             .collection('vaccines')
             .get()
             .then(function(querySnapshot) {
                 querySnapshot.forEach(function(doc) {
                     // doc.data() is never undefined for query doc snapshots
-                    // console.log(doc.id, ' => ', doc.data());
                     let data = doc.data();
+                    // get vaccine schedule
                     let vacSched = data.vac_schedule_day;
                     let vacName = data.vac_name;
-
-                    console.log(vacName, '=>', vacSched);
 
                     // add the vacSched to the baby's birthday
                     let sched = Moment(birthday)
                         .add(vacSched, 'days')
                         .format('YYYY-MM-DD');
-                    console.log(sched);
+
+                    // Save the schedule dates of the baby's vaccination
+                    db
+                        .collection('schedules')
+                        .doc(uuid)
+                        .set(
+                            {
+                                [`${vacName}`]: sched
+                            },
+                            { merge: true }
+                        )
+                        .then(function() {
+                            console.log('Document successfully written!');
+                        })
+                        .catch(function(error) {
+                            console.error('Error writing document: ', error);
+                        });
                 });
             });
     }
